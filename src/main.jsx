6f707bfd54e06dcdcad8b86f05b33b9fdbbce296
jarviswww <wangxiao0830@gmail.com>
2026-05-11 16:36:01 +0800

    feat: app initialization with auto-start GPS tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,15 @@
 import { render } from 'preact';
 import { App } from './app';
+import { startTracking } from './services/tracker';
+import { initTrackOnColdStart } from './services/trackSegment';
+import { db } from './db/index';
 import './styles/global.css';
 
+async function init() {
+  await db.open();
+  await initTrackOnColdStart();
+  startTracking();
+}
+
 render(<App />, document.getElementById('root'));
+init();
